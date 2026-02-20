e8884e60723dd17a950037db87f3e636f94abbd0
portbound <token.api.guy@gmail.com>
2026-02-20 14:56:34 -0500

    updates to waybar and hyprland

--- hypr/scripts/check_network.sh
+++ hypr/scripts/check_network.sh
@@ -2,13 +2,14 @@
 
 wifi_status=$(nmcli device status |awk '$2 == "wifi"' |awk '{print $3}')
 ethernet_status=$(nmcli device status |awk '$2 == "ethernet"' |awk '{print $3}')
+essid=$( nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2)
 
 WIFI_ICON=""
 ETHERNET_ICON="󰈀"
 DISCONNECTED_ICON="⚠"
 
 
-if [ "${wifi_status}" = "connected" ]; then echo "$WIFI_ICON  connected"
-elif [ "${ethernet_status}" = "connected" ]; then echo "$ETHERNET_ICON connected"
+if [ "${wifi_status}" = "connected" ]; then echo [ "$WIFI_ICON  ${essid} ]"
+elif [ "${ethernet_status}" = "connected" ]; then echo "$ETHERNET_ICON"
 else echo "$DISCONNECTED_ICON disconnected"
 fi
